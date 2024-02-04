8e9e9f8b716ae336d25b9ec9cae9be313153d93a
Asura0666 <dhirajlande18@gmail.com>
2024-02-04 12:23:22 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -4,28 +4,18 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
-
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
-
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
-);
-
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+// Routes imports
+import userRouter from "./routes/user.routes.js";
+
+// Routes declaration
+app.use('/api/v1/user',userRouter)
+
+// http://localhost:8080/api/v1/user/register
 
 export { app };
