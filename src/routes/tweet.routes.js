7488c8e0bf04dcd58f34369451961fee50e61c15
Asura0666 <dhirajlande18@gmail.com>
2024-02-20 23:37:24 +0530

    fix bugs

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { verifyJwtToken } from "../middlewares/auth.middlewares.js";
-import { createTweet, deleteTweet, getUsertweets, updateTweet } from "../controllers/tweet.controller.js";
+import {
+  createTweet,
+  deleteTweet,
+  getUsertweets,
+  updateTweet,
+} from "../controllers/tweet.controller.js";
 
 const router = Router();
 router.use(verifyJwtToken);
 
 router.route("/").post(createTweet);
-router.route('/user/:userId').get(getUsertweets)
-router.route('/t/:tweetId').patch(updateTweet).delete(deleteTweet)
+router.route("/user/:userId").get(getUsertweets);
+router.route("/t/:tweetId").patch(updateTweet).delete(deleteTweet);
 
 export default router;
