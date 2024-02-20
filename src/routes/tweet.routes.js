b5f991c00e3997fe6794fbdfba3eb8710913f5b6
Asura0666 <dhirajlande18@gmail.com>
2024-02-20 21:26:43 +0530

    added tweets controller and routes with testing

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyJwtToken } from "../middlewares/auth.middlewares.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, deleteTweet, getUsertweets, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router();
 router.use(verifyJwtToken);
 
 router.route("/").post(createTweet);
+router.route('/user/:userId').get(getUsertweets)
+router.route('/t/:tweetId').patch(updateTweet).delete(deleteTweet)
 
 export default router;
